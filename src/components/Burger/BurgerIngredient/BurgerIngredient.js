339a118cf646d3bc6ad41012958a7e817c2e535d
neelratanguria <neelratan@live.com>
2020-10-08 12:17:02 +0530

    add seeds

--- src/components/Burger/BurgerIngredient/BurgerIngredient.js
+++ src/components/Burger/BurgerIngredient/BurgerIngredient.js
@@ -1,11 +1,20 @@
 import React from 'react';
-import classes from './BurgerIngredient.css'
+import './BurgerIngredient.css'
 
 const burgerIngredient = (props) => {
     let ingredient = null;
 
     switch(props.type) {
-     
+     case('bread-bottom'):
+        ingredient = <div className="BreadBottom"></div>
+        break;
+    case('bread-top'):
+        ingredient = (
+            <div className="BreadTop">
+                <div className="Seed1"></div>
+                <div className="Seed2"></div>
+            </div>
+        )
     }
 };
 
